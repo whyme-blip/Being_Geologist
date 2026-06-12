404faa6c7e1544f9f759f42bdf5ca7fdce099592
whyme-blip <whymetrymenow@gmail.com>
2026-06-12 11:37:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
-const CACHE_NAME = "field-geology-v1";
+const CACHE_NAME = 'geologger-v2';
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
